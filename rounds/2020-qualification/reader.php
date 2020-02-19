34f3e8350b7ccb870be568c55e7fadfe6c53d1d3
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-02-19 21:27:27 +0100

    Added reader file skeleton

--- rounds/2020-qualification/reader.php
+++ rounds/2020-qualification/reader.php
@@ -1,1 +1,22 @@
 <?php
+
+use Utils\FileManager;
+use Utils\Log;
+
+require_once '../../bootstrap.php';
+
+/* classes */
+
+/* helper functions */
+
+/* read input */
+
+$SCORE = 0; // increment this during the solution to track the points
+
+$fileManager = new FileManager($fileName);
+$fileContent = $fileManager->get();
+
+$fileRows = explode("\n", $fileContent);
+//list(..) = explode(' ', $fileRows[0]);
+
+Log::out("Finito di leggere input", 0);
