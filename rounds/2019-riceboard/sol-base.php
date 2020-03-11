3125029d1cbbfee10a78748ff6535ab5cae86eac
Giorgio Bertolotti <giorgiobertol@gmail.com>
2020-03-11 18:31:21 +0100

    Added riceboard folder

---
+++ rounds/2019-riceboard/sol-base.php
@@ -0,0 +1,21 @@
+<?php
+
+use Utils\Log;
+use Utils\Stopwatch;
+
+require_once 'reader.php';
+
+// Runtime
+
+$SCORE = 0;
+
+Stopwatch::tik('Totale');
+
+Log::out("SCORE: " . $SCORE, 0);
+
+Stopwatch::tok('Totale');
+Stopwatch::print('Totale');
+
+// populate output array
+$output = [];
+$fileManager->output(implode("\n", $output));
